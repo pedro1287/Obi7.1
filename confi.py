7c74a95790495141f9242d9b2acc4ab93d3ddf6c
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2023-05-25 00:17:34 -0400

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,3 +1,3 @@
 Bot_token = "5895984505:AAF4h96HJ9a2zbe42ALg28tWNcAENu4zF30"
 chanel_id = -1001807928266
-db_access = 2 #value int de la db, desmarcar line 1531 bot.py
+db_access =  #value int de la db, desmarcar line 1531 bot.py
