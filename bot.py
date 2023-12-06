0d6a07540ac7a50ef3a84ec2db1dd87815715cf9
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2023-12-06 16:26:29 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ from RVClient import Rlogin
 api_id = 10181262
 api_hash = "f52b5a057b73b9974eaa7403e04907f0"
 bot_token = Bot_token
-Channel_Id = -4029916961
+Channel_Id = channel_id
 bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
 boss = ['','JAGB2021']#usuarios supremos
 
