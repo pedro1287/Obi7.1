5a22c46dac6bf61dd3281474d8e9e6f92bd4cd2c
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2024-01-08 14:55:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,16 +53,16 @@ from decorators import async_decorator
 
 from RVClient import Rlogin
 
-api_id = 11405596
+api_id = 10181262
 api_hash = "39ad6135ac9b75a47f2d1804c092c8f8"
 bot_token = Bot_token
 Channel_Id = chanel_id
 bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
-boss = ['raydel0307','Michel1203']#usuarios supremos
+boss = ['raydel0307','JAGB2021']#usuarios supremos
 
 Configs = {"uclv":'',"gtm":"","uvs":"","ltu":"", 
 			"ucuser": "", "ucpass":"","uclv_p":"", "gp":None, "s":"On", 
-			'Michel1203': {'z': 99,"m":"e","a":"c","t":"y","gp":False},
+			'JAGB2021': {'z': 99,"m":"e","a":"c","t":"y","gp":False},
 			'raydel0307': {'z': 99,"m":"e","a":"c","t":"y","gp":False}}
 
 Urls = {} #urls subidos a educa
@@ -72,8 +72,8 @@ id_de_ms = {} #id de mensage a borrar con la funcion de cancelar
 root = {} #directorio actual
 downlist = {} #lista de archivos descargados
 procesos = 0 #numero de procesos activos en el bot
-total_up = {'Michel1203':{'P':0,'S':0},'raydel0307':{'P':0,'S':0}} #total en gb o megas subidos en bytes (int)
-rvs = {'Michel1203':{'h':'','u':'','p':'','up':'','z':0,'m':'m'}}
+total_up = {'JAGB2021':{'P':0,'S':0},'raydel0307':{'P':0,'S':0}} #total en gb o megas subidos en bytes (int)
+rvs = {'JAGB2021':{'h':'','u':'','p':'','up':'','z':0,'m':'m'}}
 
 #inicio
 @bot.on_message(filters.command("start", prefixes="/") & filters.private)
@@ -83,7 +83,7 @@ async def start(client: Client, message: Message):
 	try:await get_messages()
 	except:await send_config()
 	if comprobacion_de_user(username) == False:
-		await send("⛔ 𝑵𝒐 𝒕𝒊𝒆𝒏𝒆 𝒂𝒄𝒄𝒆𝒔𝒐 @Michel1203 ⛔")
+		await send("⛔ 𝑵𝒐 𝒕𝒊𝒆𝒏𝒆 𝒂𝒄𝒄𝒆𝒔𝒐 @JAGB2021 ⛔")
 		return
 	else:pass
 	zipps = str(Configs[username]["z"])
